446c151124650d7d98688485f031cd5ff5ccb661
BYY <53412286+MGYBY@users.noreply.github.com>
2024-02-01 14:24:20 -0500

    Update wribl.py

--- WRIBLE/periodic/wribl.py
+++ WRIBLE/periodic/wribl.py
@@ -15,6 +15,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import dedalus.public as d3
 import logging
+import csv
 logger = logging.getLogger(__name__)
 
 
@@ -30,13 +31,13 @@ gw_c = 1.0+1.0/fro
 film_param = tan_theta
 disp_amp = 0.1250
 
-Nx = 1024
+Nx = 2048
 dealias = 3/2
 stop_sim_time = 80.0
 output_interval = 0.250
 timestepper = d3.RK222
 # timestepper = d3.SBDF2
-init_timestep = 0.20*0.50*(Lx/Nx)/gw_c
+init_timestep = 0.160*0.50*(Lx/Nx)/gw_c/2.0
 max_timestep = init_timestep*4.0
 dtype = np.float64
 
@@ -44,10 +45,13 @@ dtype = np.float64
 xcoord = d3.Coordinate('x')
 dist = d3.Distributor(xcoord, dtype=dtype)
 xbasis = d3.RealFourier(xcoord, size=Nx, bounds=(0, Lx), dealias=dealias)
+# xbasis = d3.RealFourier(xcoord, size=Nx, bounds=(0, Lx))
+# xbasis = d3.Chebyshev(xcoord, size=Nx, bounds=(0, Lx))
 
 # Fields
 h = dist.Field(name='h', bases=xbasis)
 q = dist.Field(name='q', bases=xbasis)
+# q = dist.VectorField(xcoord, name='q', bases=xbasis)
 
 # Substitutions
 dx = lambda A: d3.Differentiate(A, xcoord)
@@ -56,8 +60,12 @@ ve_coeff = 1.0-(5.0*deb*(1.0-lambda_r))/(2.0*rey*h*h)
 
 # Problem
 problem = d3.IVP([h,q], namespace=locals())
-problem.add_equation("dt(h) + dx(q) = 0")
-problem.add_equation("dt(q) + (1/ve_coeff)*dx((9*q*q)/(7*h)+(5)/(4*rey*tan_theta)*h*h) = (1/ve_coeff)*(q/(7*h)*dx(q) + 5/(2*film_param*rey)*(h-q/h/h) + 5*film_param*film_param*web/2/rey*h*dx(dx(dx(h))) + (film_param/rey)*(9/2*dx(dx(q)) - 9/(2*h)*dx(q)*dx(h) + (4*q)/(h*h)*(dx(h)**2) - (6*q)/h*dx(dx(h))) + ((5*deb*(1-lambda_r))/(2*rey*h**4))*(6*h*q*dx(q) - 6*q*q*dx(h)))")
+problem.add_equation("dt(h) =  -dx(q)")
+# problem.add_equation("dt(q) + (1/ve_coeff)*dx((9*q*q)/(7*h) + 5/(4*rey*tan_theta)*h*h) = (1/ve_coeff)*(q/(7*h)*dx(q) + 5/(2*film_param*rey)*(h-q/h/h) + 5*film_param*film_param*web/2/rey*h*dx(dx(dx(h))) + (film_param/rey)*(9/2*dx(dx(q)) - 9/(2*h)*dx(q)*dx(h) + (4*q)/(h*h)*(dx(h)*dx(h)) - (6*q)/h*dx(dx(h))) + ((5*deb*(1-lambda_r))/(2*rey*h**4))*(6*h*q*dx(q) - 6*q*q*dx(h)))")
+
+# a simplified version of the governing equations: no visco-elastic effect
+# problem.add_equation("dt(q) + dx((9*q*q)/(7*h) + 5/(4*rey*tan_theta)*h*h) = q/(7*h)*dx(q) + 5/(2*film_param*rey)*(h-q/h/h) + 5*film_param*film_param*web/2/rey*h*dx(dx(dx(h))) + (film_param/rey)*(9/2*dx(dx(q)) - 9/(2*h)*dx(q)*dx(h) + (4*q)/(h*h)*(dx(h)*dx(h)) - (6*q)/h*dx(dx(h)))")
+problem.add_equation("dt(q) = dx(-(9*q*q)/(7*h) - 5/(4*rey*tan_theta)*h*h) + q/(7*h)*dx(q) + 5/(2*film_param*rey)*(h-q/h/h) + 5*film_param*film_param*web/2/rey*h*dx(dx(dx(h))) + (film_param/rey)*(9/2*dx(dx(q)) - 9/(2*h)*dx(q)*dx(h) + (4*q)/(h*h)*(dx(h)*dx(h)) - (6*q)/h*dx(dx(h)))")
 
 # Initial conditions
 x = dist.local_grid(xbasis)
@@ -69,28 +77,44 @@ solver = problem.build_solver(timestepper)
 solver.stop_sim_time = stop_sim_time
 
 # Analysis
-snapshots = solver.evaluator.add_file_handler('snapshots', sim_dt=output_interval, max_writes=50)
-snapshots.add_task(h, name='depth')
-snapshots.add_task(q, name='discharge')
+# snapshots = solver.evaluator.add_file_handler('snapshots', sim_dt=output_interval, max_writes=50)
+# snapshots.add_task(h, name='depth')
+# snapshots.add_task(q, name='discharge')
 
+# use constant timestep for now
 # CFL
-CFL = d3.CFL(solver, initial_dt=init_timestep, cadence=10, safety=0.2, threshold=0.1,
-             max_change=1.5, min_change=0.5, max_dt=max_timestep)
-CFL.add_velocity(u)
+# CFL = d3.CFL(solver, initial_dt=init_timestep, cadence=10, safety=0.2, threshold=0.1,
+#              max_change=1.5, min_change=0.5, max_dt=max_timestep)
+# CFL.add_velocity(u)
 
 # Main loop
-try:
-    logger.info('Starting main loop')
-    while solver.proceed:
-        timestep = CFL.compute_timestep()
-        solver.step(timestep)
-        if solver.iteration % 10 == 0:
-        logger.info('Iteration=%i, Time=%e, dt=%e' %(solver.iteration, solver.sim_time, timestep))
-except:
-    logger.error('Exception raised, triggering end of main loop.')
-    raise
-finally:
-    solver.log_stats()
+# try:
+#     logger.info('Starting main loop')
+#     while solver.proceed:
+#         timestep = CFL.compute_timestep()
+#         solver.step(timestep)
+#         if solver.iteration % 10 == 0:
+#             logger.info('Iteration=%i, Time=%e, dt=%e' %(solver.iteration, solver.sim_time, timestep))
+# except:
+#     logger.error('Exception raised, triggering end of main loop.')
+#     raise
+# finally:
+#     solver.log_stats()
+
+h.change_scales(1)
+q.change_scales(1)
+# u_list = [np.copy(u['g'])]
+# t_list = [solver.sim_time]
+while solver.proceed:
+    solver.step(init_timestep)
+    if solver.iteration % 10 == 0:
+        logger.info('Iteration=%i, Time=%e, dt=%e' %(solver.iteration, solver.sim_time, init_timestep))
+    if solver.iteration % 100 == 0:
+        format_string_time = f"{solver.iteration:d}"
+        file_name = 'outXYZ_%s' % format_string_time
+        with open(file_name, 'w') as f:
+            writer = csv.writer(f, delimiter='\t')
+            writer.writerows(zip(np.transpose(x), np.transpose(h['g']), np.transpose(q['g'])))
 
 # Plot
 # plt.figure(figsize=(6, 4))
